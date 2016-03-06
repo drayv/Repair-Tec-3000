1bb6af4e4c64aa3e875d935481d2bf951b58cf37
drayv <scrib@yandex.ru>
2016-03-06 14:35:20 +0200

    fixed angular DI bug for minification js

--- RepTec/App/services.js
+++ RepTec/App/services.js
@@ -2,17 +2,21 @@
 
 var services = angular.module('repTec.services', ['ngResource']);
 
-services.factory('RepairersFactory', function ($resource) {
-    return $resource('/api/Repairers', {}, {
-        query: { method: 'GET', isArray: true },
-        create: { method: 'POST' }
-    })
-});
+services.factory('RepairersFactory', ['$resource',
+    function ($resource) {
+        return $resource('/api/Repairers', {}, {
+            query: { method: 'GET', isArray: true },
+            create: { method: 'POST' }
+        })
+    }
+]);
 
-services.factory('RepairerFactory', function ($resource) {
-    return $resource('/api/Repairers/:id', {}, {
-        show: { method: 'GET' },
-        update: { method: 'PUT', params: { id: '@id' } },
-        delete: { method: 'DELETE', params: { id: '@id' } }
-    })
-});
+services.factory('RepairerFactory', ['$resource',
+    function ($resource) {
+        return $resource('/api/Repairers/:id', {}, {
+            show: { method: 'GET' },
+            update: { method: 'PUT', params: { id: '@id' } },
+            delete: { method: 'DELETE', params: { id: '@id' } }
+        })
+    }
+]);
